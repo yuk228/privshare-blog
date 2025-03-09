71a699c8d95886160a40aea7bc1527491ecdbc24
yuk228 <joyful.nachos-0w@icloud.com>
2025-03-10 01:12:35 +0900

    💄 Improve Style

--- app/blogs/anonimity/[slug]/page.tsx
+++ app/blogs/anonimity/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import fs from "fs"
 import Image from "next/image";
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { getPostData } from "@/lib/blogs/getPostData";
 
 export default async function Home({ params }: { params: Promise<{slug: string}>}) {
@@ -12,9 +13,10 @@ export default async function Home({ params }: { params: Promise<{slug: string}>
               <Image alt={post.frontMatter.title} src={post.frontMatter.img || ""} width={400} height={200} className="mx-auto"/>
               <h1 className="text-4xl font-bold py-5 text-left md:text-center lg:text-center">{post.frontMatter.title}</h1>
               <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
               components={{
                 h2: ({...props }) => <h2 className="my-3 mt-[30px] pl-3 text-2xl font-bold border-l-2 border-gray-500" {...props} />,
-                p: ({...props }) => <p className="ml-2" {...props} />,
+                p: ({...props }) => <p className="ml-2 pb-" {...props} />,
                 li: ({...props }) => <li className="ml-7 my-2 pl-3 border-l-2 border-gray-500" {...props} />,
                 a: ({...props }) => <a className="text-blue-700" {...props} />,
                 code: ({...props }) => <code className="bg-gray-900" {...props} />,
