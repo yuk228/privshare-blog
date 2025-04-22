4fc9eb798187fa1db7ad6d70d29bb7533a3d7059
yuk228 <joyful.nachos-0w@icloud.com>
2025-04-22 18:08:05 +0900

    📱 Responsive

--- app/blogs/other/page.tsx
+++ app/blogs/other/page.tsx
@@ -4,7 +4,7 @@ import BlogList from '@/components/blog/BlogList'
 export default async function Home() {
     return (
         <main>
-            <div className="min-h-screen bg-[#030303] px-[50px] py-[100px] md:px-[200px] lg:px-[500px] ">
+            <div className="min-h-screen px-[50px] py-[100px] md:px-[100px] lg:px-[250px]">
                 <BlogList deviceName="other"/>
             </div>
         </main>
