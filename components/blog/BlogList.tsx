ea9fbf01a41fd5ef0fd30413b06fff5b71c370a6
Yuk <194586153+yuk228@users.noreply.github.com>
2025-06-02 18:20:12 +0900

    📱 Responsive for blog tags

--- components/blog/BlogList.tsx
+++ components/blog/BlogList.tsx
@@ -13,7 +13,13 @@ const BlogList = async (props: { tag?: string }) => {
                         <Image alt={post.FrontMatter.title} src={post.FrontMatter.img || "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.ucMk47FSeRx5WqeIX-wgmAHaEH%26pid%3DApi&f=1&ipt=cf72bcee25c62444c38461f26f5bb405f4181a62d655c8190a64d7d32ec9c247&ipo=images"} width={400} height={200} className="mx-auto pb-2 object-cover" />
                         <h2 className="text-xl font-semibold text-gray-800 dark:text-white">{post.FrontMatter.title}</h2>
                         <p className="mt-2 text-gray-600 dark:text-gray-400">{post.FrontMatter.description || ""}</p>
-                        <p className="mt-2 text-gray-600 dark:text-gray-400">{post.FrontMatter.tags?.join(", ") || ""}</p>
+                        <div className="grid grid-cols-1 lg:grid-cols-3 mt-2 gap-2">
+                            {post.FrontMatter.tags?.map((tag) => (
+                                <div className="border hover:border-muted-foreground transition-all duration-300 rounded-md px-3 py-2 text-center" key={tag}>
+                                    {tag}
+                                </div>
+                            ))}
+                        </div>
                     </Link>
                 </div>
             ))}
