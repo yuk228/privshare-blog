33e0715bac013b975c3dc07fbc40025722b31b09
Yuk <194586153+yuk228@users.noreply.github.com>
2025-06-02 18:12:52 +0900

    🐛 Fix 500 error

--- app/blogs/tags/[tag]/page.tsx
+++ app/blogs/tags/[tag]/page.tsx
@@ -1,27 +1,46 @@
 import fs from "fs"
 import { getPostData } from "@/lib/blogs/getPostData";
 import BlogList from "@/components/blog/BlogList";
+import { notFound } from 'next/navigation';
 
 export default async function Home({ params }: { params: Promise<{tag: string}>}) {
   const { tag } = await params;
-  return (
-    <main>
-    <div className="min-h-screen px-[50px] py-[100px] md:px-[100px] lg:px-[250px]">
-        <BlogList tag={tag}/>
-    </div>
-</main> 
-  );
+  try {
+    const files = fs.readdirSync("./app/blogs/posts", "utf-8");
+    const slugs = files.map((file) => file.replace(/\.md$/, ''));
+    const posts = await Promise.all(slugs.map(async (slug) => {
+      const post = await getPostData(slug);
+      return post;
+    }));
+    
+    const filteredPosts = posts.filter(post => post.frontMatter.tags?.includes(tag));
+    
+    if (filteredPosts.length === 0) {
+      notFound();
+    }
+
+    return (
+      <main>
+        <div className="min-h-screen px-[50px] py-[100px] md:px-[100px] lg:px-[250px]">
+          <BlogList tag={tag}/>
+        </div>
+      </main> 
+    );
+  } catch {
+    notFound();
+  }
 }
 
 export async function generateStaticParams() {
   const files = fs.readdirSync("./app/blogs/posts", "utf-8");
-  const slugs = await files.map((file) => file.replace(/\.md$/, ''));
-  console.log(slugs);
-  const tags = slugs.map(async (slug) => {
+  const slugs = files.map((file) => file.replace(/\.md$/, ""));
+  const posts = await Promise.all(slugs.map(async (slug) => {
     const post = await getPostData(slug);
     return post.frontMatter.tags;
-  });
+  }));
+  
+  const tags = posts.flat().filter((tag): tag is string => tag !== undefined);
   return tags.map((tag) => ({
-    tags: tag,
+    tag,
   }));
 }
