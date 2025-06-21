39496bff4132a36b36d6ba7389b554758c303442
yuk228 <joyful.nachos-0w@icloud.com>
2025-06-22 00:54:32 +0900

    💄 Support Dark / Light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,9 +32,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-          forcedTheme="dark"
+          defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
         >
           <Header />
