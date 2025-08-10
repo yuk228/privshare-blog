3be6c9ebaa2f9791bcce936393d0224989f5cc78
yuk228 <joyful.nachos-0w@icloud.com>
2025-08-11 02:31:40 +0900

    ..

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "prisma/**/*",
+      "**/*.prisma",
+      "node_modules/**/*",
+      "generated/**/*",
+      "app/generated/**/*",
+      "**/generated/**/*",
+    ],
+  },
 ];
 
 export default eslintConfig;
